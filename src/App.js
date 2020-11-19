85bc44711660f916d2f3a75010d3f859aa973b64
vitodigrigoli <73437231+vitodigrigoli@users.noreply.github.com>
2020-11-19 20:29:57 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
 import React from "react";
 import './App.css';
-import Header from "./Components/Header";
-import Slider from "./Components/Slider";
-
 
 function App() {
 
-	const filmList = [
-		{
-			title: 'Titolo 1',
-			image: '/Images/the_witcher.jpg'
-		},
-		{
-			title: 'Titolo 2',
-			image: '/Images/the_witcher.jpg'
-		},
-		{
-			title: 'Titolo 3',
-			image: '/Images/the_witcher.jpg'
-		},
-		{
-			title: 'Titolo 3',
-			image: '/Images/the_witcher.jpg'
-		},
-		{
-			title: 'Titolo 3',
-			image: '/Images/the_witcher.jpg'
-		},
-		{
-			title: 'Titolo 3',
-			image: '/Images/the_witcher.jpg'
-		}
-	]
 	return (
 		<div className="App">
-			<Header/>
-			<Slider title={'I più visti'} list={filmList}/>
+
 		</div>
 	);
 }
